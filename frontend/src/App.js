1b709aa4722e085f3b23d2854011a6a4f8cba250
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-07 11:37:39 +0700

    create basic routes with react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-console.log(BrowserRouter);
+const Home = () => <h1>Home</h1>;
 
-const App = () => <h1>Hello World</h1>;
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={Home} />
+    </div>
+  </Router>
+);
 
 export default App;
