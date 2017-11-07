ed4b996313c3bb4a42333dff990bb5ced7fa6d54
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-07 11:48:49 +0700

    end lesson 3 navigate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const Home = () => <h1>Home</h1>;
+import "./App.css";
 
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to={{ pathname: "/about" }}> About</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+);
 const App = () => (
   <Router>
     <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={Home} />
+      <Links />
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route path="/about" render={() => <h1>About</h1>} />
+      <Route path="/contact" render={() => <h1>Contact</h1>} />
     </div>
   </Router>
 );
